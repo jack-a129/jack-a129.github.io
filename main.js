8edf515fefae23c839b6effca786c5bff5c720df
浅倉有人 <113231783+jack-a129@users.noreply.github.com>
2025-08-20 22:21:24 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+let i = 3;
+
 function say_otsu(){
-    document.getElementById("otsu").innerText = "しごおつ";
+    const tan = "タン";
+    let text = "";
+    for (let x = 0; x < i; x++){
+        text = text + tan;
+    }
+    console.log(text) 
+    document.getElementById("otsu").innerText = text + "麺";
+    i += 1;
 }
